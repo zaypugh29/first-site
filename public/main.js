7cfe796fa768faff1e14a123df6b6a2e6eb2a31f
zay <zaypugh29@gmail.com>
2023-12-03 16:13:23 -0500

    now?

---
+++ public/main.js
@@ -0,0 +1,21 @@
+"use strict";
+(() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // <stdin>
+  var require_stdin = __commonJS({
+    "<stdin>"(exports) {
+      Object.defineProperty(exports, "__esModule", { value: true });
+      exports.main = void 0;
+      function main() {
+        console.log("Hello World!");
+      }
+      exports.main = main;
+      main();
+    }
+  });
+  require_stdin();
+})();
