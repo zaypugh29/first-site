2e16992f1615f65389a1c884183fb075c08eabd6
zay <zaypugh29@gmail.com>
2023-12-03 14:23:19 -0500

    add ts

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+function main() {
+    if (document.body.id) {
+        console.log(document.body.id);
+    }
+}
+exports.main = main;
+main();
---
+++ ts/main.ts
@@ -0,0 +1,6 @@
+export function main () {
+    if (document.body.id) {
+        console.log(document.body.id);
+    }
+}
+main();
